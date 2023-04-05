e92722abf02c72ac940bfd37007c29e482c6361a
Your Name <you@exle.com>
2023-04-05 17:17:37 -0600

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './gestion/pages/inicio/inicio.component';
 
 const routes: Routes = [
   {path: 'inicio', loadChildren: () => import ('./gestion/gestion.module').then (m => m.GestionModule)},
   {path: 'auth', loadChildren: () => import ('./auth/auth.module').then (m => m.AuthModule)},
   
   {path: '**', redirectTo: 'inicio'},
-  {path: '**', redirectTo: 'login'},
+  {path: '**', redirectTo: 'auth'},
 ];
 
 @NgModule({
